e64ed727772638492c5074297049755b13710a4c
codedtrap <salahucknn@gmail.com>
2020-07-29 19:57:23 +0530

    Added Startup Message

--- start-game.sh
+++ start-game.sh
@@ -2,6 +2,7 @@
 
 # this bash script is for simplifying the interface for GuessWord game.
 # please run 'bash start-game.sh' for starting the script
+echo "Please Wait..The Game Will Start Shortly"
 
 cd src_files       # navigating to source files
 pip install -r requirements.txt # installing python dependencies
